e4fcc5acc48ab93feb8e79826786a710c1d874af
Louis <53097306+louisjdcharles@users.noreply.github.com>
2022-08-30 19:40:31 +0100

    fixed config errors and enabled trpc ssr

--- next.config.mjs
+++ next.config.mjs
@@ -15,13 +15,5 @@ function defineNextConfig(config) {
 
 export default defineNextConfig({
   reactStrictMode: true,
-  swcMinify: true,
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300
-    }
-
-    return config
-  }
+  swcMinify: true
 });
